9ec5575990a93f382f09d24c54cd9d19f6bfcc03
Paulo Prata <paulodanielprata@gmail.com>
2021-10-05 20:20:36 -0300

    create controller companies applicants

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  resources :companies, only: [:new, :edit, :update, :create]
+  resources :positions do
+  resources :applicants, only: [:index]
+  end
+  resources :applicants, only: [:new, :create]
   devise_for :users
   root "home#index"
+  #get "/vaga/:slug", action: :public_position, controller: :positions, as: :public_position
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
