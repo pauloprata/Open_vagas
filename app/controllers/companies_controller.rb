9af619f90039e227fd75617ebd58c6f2808c2034
Paulo Prata <paulodanielprata@gmail.com>
2021-10-05 21:05:42 -0300

    create views companies

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,7 +1,28 @@
 class CompaniesController < ApplicationController
   def new
+    @company = current_user.build_company
   end
 
   def edit
+  end
+
+  def create
+    @company = current_user.build_company(company_params)
+    if @company.save
+      flash[:notice] = 'Empresa cadastrada com sucesso'
+      redirect_to root_path
+    else 
+      flash[:error] = 'Erro ao Cadastrar a Empresa'
+      render :new
+    end
+  end
+
+  def update
+  end
+
+  private 
+
+  def company_params 
+    params.require(:company).permit(:nam, :url, :logo)
   end
 end
