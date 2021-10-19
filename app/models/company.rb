8cd744c00fc87ca7ca2a861e8510900b13fd4cf9
Paulo Prata <paulodanielprata@gmail.com>
2021-10-18 21:37:32 -0300

    controller e views positions

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,6 @@
 class Company < ApplicationRecord
   belongs_to :user
+  has_many :positions
   has_one_attached :logo
   validates :url, presence: true
   validates :name, presence: true
--- app/models/position.rb
+++ app/models/position.rb
@@ -1,3 +1,6 @@
 class Position < ApplicationRecord
   belongs_to :company
+  enum career: [:developer, :business_inteligence, :information_technology, :design, 
+    :product, :technology, :other]
+  enum contract: [:clt, :pj, :match]
 end
